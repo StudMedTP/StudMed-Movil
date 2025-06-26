45a6cab04db0edf8d321fd9aa889cacf7d189cc8
Tomas Ortiz <u201910146@upc.edu.pe>
2025-06-26 14:19:28 -0500

    First Commit

---
+++ app/src/main/java/com/example/studmed/Docente/MainActivityDocente.kt
@@ -0,0 +1,16 @@
+package com.example.studmed.Docente
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.studmed.R
+
+class MainActivityDocente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_docente)
+
+    }
+}
