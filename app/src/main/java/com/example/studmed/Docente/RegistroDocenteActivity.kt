24ce3930d2e919eb421682e6b2091b636de74578
Tomas Ortiz <u201910146@upc.edu.pe>
2025-06-27 00:00:12 -0500

    feat: Register Docente

---
+++ app/src/main/java/com/example/studmed/Docente/RegistroDocenteActivity.kt
@@ -0,0 +1,14 @@
+package com.example.studmed.Docente
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import com.example.studmed.R
+
+class RegistroDocenteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_docente)
+
+    }
+}
